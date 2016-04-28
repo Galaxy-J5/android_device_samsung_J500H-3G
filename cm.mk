45a2090c2f3bb0bb4e289fc78010db0ab6997955
Ganesh Varma <ganeshvarmai4u@gmail.com>
2016-04-28 23:09:56 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,8 +1,8 @@
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-$(call inherit-product, device/samsung/j5ltexx/full_j5ltexx.mk)
+$(call inherit-product, device/samsung/j5ltexx/full_j500h.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=j5ltexx TARGET_DEVICE=j5lte
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=j500h TARGET_DEVICE=j500h
 
-PRODUCT_NAME := cm_j5ltexx
+PRODUCT_NAME := cm_j500h
