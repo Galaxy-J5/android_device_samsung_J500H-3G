61bbca3ae019959280aeed09dec4997147903f62
Ganesh Varma <ganeshvarmai4u@gmail.com>
2016-04-28 23:07:14 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,4 +32,4 @@ TARGET_KERNEL_CONFIG := cyanogenmod_j5nlte_defconfig
 # Vendor Init
 TARGET_UNIFIED_DEVICE := true
 TARGET_INIT_VENDOR_LIB := libinit_msm
-TARGET_LIBINIT_DEFINES_FILE := device/samsung/j5ltexx/init/init_j5lte.c
+TARGET_LIBINIT_DEFINES_FILE := device/samsung/j500h/init/init_j500h.c
