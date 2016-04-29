9b0315533620a96c31995601422b7ba74f856a79
Nick Verse <nikoskalomiris@gmail.com>
2016-04-29 05:03:32 +0300

    Update and rename full_j500h.mk to full_j53gxx.mk

--- full_j53gxx.mk
+++ full_j53gxx.mk
@@ -15,11 +15,11 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from j500h device
-$(call inherit-product, device/samsung/j500h/device.mk)
+# Inherit from j53g device
+$(call inherit-product, device/samsung/j53gxx/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_j500h
-PRODUCT_DEVICE := j500h
+PRODUCT_NAME := full_j53gxx
+PRODUCT_DEVICE := j53gxx
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
