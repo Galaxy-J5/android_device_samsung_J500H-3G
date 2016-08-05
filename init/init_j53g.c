cac3013d08441a0f8799199225356eaa53927a62
Nick Verse <nikoskalomiris@gmail.com>
2016-08-05 16:12:46 +0300

    Reverted to Nepal variant to avoid issues with TWRP.

--- init/init_j53g.c
+++ init/init_j53g.c
@@ -62,8 +62,8 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
 
    if (strstr(bootloader, "J500H")) {
       /* SM-J500H */
-      property_set("ro.build.fingerprint", "samsung/j53gxx/j53g:5.1.1/LMY48B/J500HXXS1APE1:user/release-keys");
-      property_set("ro.build.description", "j53gxx-user 5.1.1 LMY48B J500HXXS1APE1 release-keys");
+      property_set("ro.build.fingerprint", "samsung/j53gxx/j53g:5.1.1/LMY48B/J500HXXU1APC2:user/release-keys");
+      property_set("ro.build.description", "j53gxx-user 5.1.1 LMY48B J500HXXU1APC2 release-keys");
       property_set("ro.product.model", "SM-J500H");
       property_set("ro.product.device", "j53g");
 
